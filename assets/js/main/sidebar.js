15f7803c7a3d2ef7a304428c733790afaa0ffabd
madr3 <m.adriansyah3@gmail.com>
2024-10-14 20:53:48 -0400

    Revised collapsed sidebar and added js for expanded home-content

--- assets/js/main/sidebar.js
+++ assets/js/main/sidebar.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     const sidebar = document.getElementById('sidebar');
     const toggleBtn = document.getElementById('btn-close');
+    const homeContent = document.querySelector('.home-content');
 
     toggleBtn.addEventListener('click', function(){
         sidebar.classList.toggle("collapsed"); //Toggles collapsed class in css
+        homeContent.classList.toggle("expanded");
 }); 
 })
+
+
