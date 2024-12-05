1b44d79d53ac22abed75803ab6468f7643c45e16
Spatel3062 <shraddhapatel3062@gmail.com>
2024-12-05 21:26:32 +0300

    Charts Addition
    Add charts and  refines some parts of the code.

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const ncrFormsRoutes = require('./routes/ncrForms'); 
-const productsRoutes = require('./routes/products');
-const suppliersRoutes = require('./routes/suppliers');
-const employeesRoutes = require('./routes/employees');
-const positionsRoutes = require('./routes/positions');
-const qualityFormsRoutes = require('./routes/qualityForms'); 
-const engineerFormsRoutes = require('./routes/engineerForms'); 
-const purchasingFormsRoutes = require('./routes/purchasingForms'); 
-const statusRoutes = require('./routes/status'); 
-const emailRoutes = require('./routes/emailRoutes');
-const pdfRoutes = require('./routes/ncrPdfRoute.js');
-const notificationsRoutes = require('./routes/notifications'); 
-
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const ncrFormsRoutes = require("./routes/ncrForms");
+const productsRoutes = require("./routes/products");
+const suppliersRoutes = require("./routes/suppliers");
+const employeesRoutes = require("./routes/employees");
+const positionsRoutes = require("./routes/positions");
+const qualityFormsRoutes = require("./routes/qualityForms");
+const engineerFormsRoutes = require("./routes/engineerForms");
+const purchasingFormsRoutes = require("./routes/purchasingForms");
+const statusRoutes = require("./routes/status");
+const emailRoutes = require("./routes/emailRoutes");
+const pdfRoutes = require("./routes/ncrPdfRoute.js");
+const notificationsRoutes = require("./routes/notifications");
+const ncrEmployeeRoutes = require("./routes/ncrEmployee");
 
 const app = express();
 
@@ -21,23 +21,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // JSON middleware to parse JSON bodies
 app.use(express.json());
 
-app.use('/api/ncrForms', ncrFormsRoutes); // Route for NCR Forms
-app.use('/api/products', productsRoutes); // Route for Products
-app.use('/api/suppliers', suppliersRoutes); // Route for Suppliers
-app.use('/api/employees', employeesRoutes); // Route for Employees
-app.use('/api/positions', positionsRoutes); // Route for Positions
-app.use('/api/qualityForms', qualityFormsRoutes); // Route for Quality Forms
-app.use('/api/engineerForms', engineerFormsRoutes); // Route for Engineer Forms
-app.use('/api/purchasingForms', purchasingFormsRoutes); // Route for Purchasing Forms
-app.use('/api/status', statusRoutes); // Route for Stanpm tus
-app.use('/api/email', emailRoutes);
-app.use('/api/ncrPdfRoute', pdfRoutes);
-app.use('/api/notifications', notificationsRoutes);
+app.use("/api/ncrForms", ncrFormsRoutes); // Route for NCR Forms
+app.use("/api/products", productsRoutes); // Route for Products
+app.use("/api/suppliers", suppliersRoutes); // Route for Suppliers
+app.use("/api/employees", employeesRoutes); // Route for Employees
+app.use("/api/positions", positionsRoutes); // Route for Positions
+app.use("/api/qualityForms", qualityFormsRoutes); // Route for Quality Forms
+app.use("/api/engineerForms", engineerFormsRoutes); // Route for Engineer Forms
+app.use("/api/purchasingForms", purchasingFormsRoutes); // Route for Purchasing Forms
+app.use("/api/status", statusRoutes); // Route for Stanpm tus
+app.use("/api/email", emailRoutes);
+app.use("/api/ncrPdfRoute", pdfRoutes);
+app.use("/api/notifications", notificationsRoutes);
+
+app.use("/api/ncrEmployee", ncrEmployeeRoutes);
 
 // Serve the HTML page for NCR Forms
 // app.get('/ncrForms', (req, res) => {
@@ -45,20 +47,25 @@ app.use('/api/notifications', notificationsRoutes);
 // });
 
 // Dynamic route to render other HTML pages
-app.get('*', (req, res) => {
-    const requestedPath = req.path === '/' ? '/login' : req.path; 
-    const sanitizedPath = requestedPath.replace('.html', '');
-    const filePath = path.join(__dirname, 'public', 'views', `${sanitizedPath}.html`);
+app.get("*", (req, res) => {
+  const requestedPath = req.path === "/" ? "/login" : req.path;
+  const sanitizedPath = requestedPath.replace(".html", "");
+  const filePath = path.join(
+    __dirname,
+    "public",
+    "views",
+    `${sanitizedPath}.html`
+  );
 
-    fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            return res.status(404).send('404 Not Found');
-        }
-        res.sendFile(filePath);
-    });
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      return res.status(404).send("404 Not Found");
+    }
+    res.sendFile(filePath);
+  });
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
